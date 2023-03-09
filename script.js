62b1f3bb8c276ab0096a8fa7cceb28ee2d7a56a5
Vijay Deshmukh <107369378+vijaydeshmukh18@users.noreply.github.com>
2023-03-09 15:15:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add smooth scrolling to nav links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
